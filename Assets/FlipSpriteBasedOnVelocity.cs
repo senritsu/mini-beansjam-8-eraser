20997f5124438ea69cf0cd865ee433d7e090f8a4
senritsu <teh.b4k4@gmail.com>
2022-06-04 19:49:23 +0200

    :lipstick: add player sprite

--- Assets/FlipSpriteBasedOnVelocity.cs
+++ Assets/FlipSpriteBasedOnVelocity.cs
@@ -4,15 +4,17 @@ public class FlipSpriteBasedOnVelocity : MonoBehaviour
 {
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody2D;
+    public bool defaultRightwards;
+    public SpriteRenderer target;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer = target ? target : GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        _spriteRenderer.flipX = _rigidbody2D.velocity.x >= 0;
+        _spriteRenderer.flipX = defaultRightwards ? _rigidbody2D.velocity.x < 0 : _rigidbody2D.velocity.x >= 0;
     }
 }
